8106e35cce69d4c40d09d3cd1dff341e42d369e9
22bipp22 <tammibippert@gmail.com>
2020-10-23 16:35:38 -0500

    first attempt at crating chart

--- D3_data_journalism/assets/js/app.js
+++ D3_data_journalism/assets/js/app.js
@@ -27,40 +27,63 @@ d3.csv('assets/data/data.csv').then(data => {
     // let xAxis = 'State'
     // let yAxis = 'Poverty'
 
-    let povertyData = data.map(entry => entry.poverty)
-    let stateAbbr = data.map(entry => entry.abbr)
-    let healthCare = data.map(entry => entry.healthcare)
+    data.forEach(function(data) {
+        data.poverty = +data.poverty;
+        data.healthcare = +data.healthcare;
+    }); 
 
-    console.log(povertyData)
-    console.log(stateAbbr)
-    console.log(healthCare)
+    // let povertyData = data.map(entry => entry.poverty)
+    // let stateAbbr = data.map(entry => entry.abbr)
+    // let healthCare = data.map(entry => entry.healthcare)
 
+    // console.log(povertyData)
+    // console.log(stateAbbr)
+    // console.log(healthCare)
+
+    //Create scales
     let xLinearScale = d3.scaleLinear()
-        .domain(d3.extent(povertyData))
+        .domain([0, d3.max(data, d => d.poverty)])
         .range([0, width])
 
     let yLinearScale = d3.scaleLinear()
-        .domain([0, d3.max(healthCare)])
+        .domain([0, d3.max(data, d => d.healthcare)])
         .range([height, 0])
 
     let bottomAxis = d3.axisBottom(xLinearScale);
     let leftAxis = d3.axisLeft(yLinearScale);
 
-    let xAxis = chartGroup.append("g")
-        .classed("x-axis", true)
+    // let xAxis = chartGroup.append("g")
+    //     .classed("x-axis", true)
+    //     .attr("transform", `translate(0, ${height})`)
+    //     .call(bottomAxis);
+
+    chartGroup.append("g")
         .attr("transform", `translate(0, ${height})`)
         .call(bottomAxis);
 
     chartGroup.append("g")
         .call(leftAxis);
 
+    // line generator
+    let line = d3.line()
+        .x(d => xLinearScale(d.poverty))
+        .y(d => yLinearScale(d.healthcare));
+
+    // create path
+    chartGroup.append("path")
+        .data([data])
+        .attr("d", line)
+        .attr("fill", "none")
+        .attr("stroke", "blue");
+
+
     let circlesGroup = chartGroup.selectAll("circle")
         // .selectAll('dot')
         .data(data)
         .enter()
         .append('circle')
-            .attr('cx', d => xLinearScale(d[stateAbbr]))
-            .attr('cy', d => yLinearScale(d[povertyData]))
+            .attr('cx', d => xLinearScale(d.poverty))
+            .attr('cy', d => yLinearScale(d.healthcare))
             .attr('r', 2.0)
             .style('fill', '#000000')
             
